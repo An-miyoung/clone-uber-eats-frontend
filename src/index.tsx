79246f9029745ba69ab121c655f945c1842e12c1
miyoung <noonchicat@gmail.com>
2023-05-15 23:20:29 +0900

    grapql setup error

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { ApolloProvider } from "@apollo/client";
+import { client } from "./apollo";
 import "./styles/styles.css";
 
 const root = ReactDOM.createRoot(
@@ -9,7 +11,9 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>
 );
 
